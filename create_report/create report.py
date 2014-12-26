9dbf5569ee1be8670ae1f3e2e8c2a0b9eb68628e
sve-openerp <sve@odoo.com>
2014-12-26 10:25:35 +0100

    [IMP] New features for create_report
    - Usability of wizard form view improved ( required fields, footer, tooltips )
    - Add a feature to add a suffix in the view name
    - Add a feature to choose whether to fill the field field of the created views
    - Fix: When creating non-translatable reports, there was no iteration on the objects and the o variable was not set.

--- create_report/create report.py
+++ create_report/create report.py
@@ -3,43 +3,9 @@ view_mod = pool['ir.ui.view']
 report_mod = pool['ir.actions.report.xml']
 module = "__custo__"
 
-if object.x_type == 'internal':
-    layout_tname = 'report.internal_layout'
-elif object.x_type == 'external':
-    layout_tname = 'report.external_layout'
+default_content = """<div class="page">
 
-# The report_name is going to be constructed as a concatenation of:
-# - The name of the module ( by default __custo__ )
-# - A dot
-# - The word "report"
-# - An underscore
-# - The name of the model, removed from any non strictly alphanumeric character
-# - An underscore
-# - The user-friendly name of the report, removed from any non strictly
-#   alphanumeric character
-# - An underscore
-# - document
-# Ex: __custo__.report_saleorder_orderconfirmation_document
-
-accepted_chars = map(chr, range(ord('a'), ord('z') + 1)) + \
-    map(chr, range(ord('0'), ord('9') + 1))
-
-name_short = ''.join(c for c in object.x_name.lower() if c in accepted_chars)
-model_short = ''.join(
-    c for c in object.x_model_id.model.lower() if c in accepted_chars)
-
-technical_name = 'report_' + model_short + '_' + name_short
-
-document_technical_name = technical_name + '_document'
-
-document_tname = module + '.' + document_technical_name
-
-document_qweb = """<?xml version="1.0"?>
-<t t-name="%s">
-    <t t-call="%s">
-        <div class="page">
-
-            <h2>Title of your report</h2>
+            <h2>Title of your report for <span t-field="o.display_name"/></h2>
 
             <div class="row mt32 mb32" id="information">
                 <div class="col-xs-3">
@@ -76,17 +42,66 @@ document_qweb = """<?xml version="1.0"?>
                 <span>Note</span>
             </p>
 
-        </div>
+        </div>"""
+
+if object.x_type == 'internal':
+    layout_tname = 'report.internal_layout'
+elif object.x_type == 'external':
+    layout_tname = 'report.external_layout'
+
+suffix = object.x_suffix or ""
+
+# The report_name is going to be constructed as a concatenation of:
+# - The name of the module ( by default __custo__ )
+# - A dot
+# - The word "report"
+# - An underscore
+# - The name of the model, removed from any non strictly alphanumeric character
+# - An underscore
+# - The user-friendly name of the report, removed from any non strictly
+#   alphanumeric character
+# - An underscore
+# - document
+# Ex: __custo__.report_saleorder_orderconfirmation_document
+
+accepted_chars = map(chr, range(ord('a'), ord('z') + 1)) + \
+    map(chr, range(ord('0'), ord('9') + 1))
+
+name_short = ''.join(c for c in object.x_name.lower() if c in accepted_chars)
+model_short = ''.join(
+    c for c in object.x_model_id.model.lower() if c in accepted_chars)
+
+technical_name = 'report_' + model_short + '_' + name_short
+
+document_technical_name = technical_name + '_document'
+
+document_tname = module + '.' + document_technical_name
+
+if object.x_translate:
+    document_qweb = """<?xml version="1.0"?>
+<t t-name="%s">
+    <t t-call="%s">
+        %s
     </t>
-</t>
-""" % (document_tname, layout_tname)
+</t>""" % (document_tname, layout_tname, default_content)
+else:
+    document_qweb = """<?xml version="1.0"?>
+<t t-name="%s">
+    <t t-call="report.html_container">
+        <t t-foreach="docs" t-as="o">
+            <t t-call="%s">
+                %s
+            </t>
+        </t>
+    </t>
+</t>""" % (document_tname, layout_tname, default_content)
 
 # Create the "document" QWeb view,
 document_view_values = {
-    'name': document_technical_name,
-    'model': 'ir.ui.view',
+    'name': document_technical_name + suffix,
     'type': 'qweb',
     'arch': document_qweb,
+    'model': object.x_model_id.model if object.x_model_in_view else False
 }
 document_view_id = view_mod.create(
     cr, uid, document_view_values, context=context)
@@ -112,10 +127,10 @@ if object.x_translate:
 
     # Create the "caller" or "translator" QWeb view,
     translate_view_values = {
-        'name': technical_name,
-        'model': 'ir.ui.view',
+        'name': technical_name + suffix,
         'type': 'qweb',
         'arch': translate_qweb,
+        'model': object.x_model_id.model if object.x_model_in_view else False
     }
     translate_view_id = view_mod.create(
         cr, uid, translate_view_values, context=context)
